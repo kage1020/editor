e29cc3ec56b0ec8f16fb609e513095974b4c4ecd
kage1020 <4624528@ed.tus.ac.jp>
2025-08-26 17:43:53 +0900

    revert

--- src/app/api/auth/[...all]/route.tsx
+++ src/app/api/auth/[...all]/route.tsx
@@ -1,6 +1,4 @@
 import { toNextJsHandler } from "better-auth/next-js"
 import { auth } from "@/auth/server"
 
-export const runtime = "edge"
-
 export const { GET, POST } = toNextJsHandler(auth.handler)
