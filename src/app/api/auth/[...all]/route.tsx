3a190030d0c9b65cf5c60516efe142f3bb1dd6af
kage1020 <4624528@ed.tus.ac.jp>
2025-08-26 17:33:13 +0900

    add edge runtime

--- src/app/api/auth/[...all]/route.tsx
+++ src/app/api/auth/[...all]/route.tsx
@@ -1,4 +1,6 @@
 import { toNextJsHandler } from "better-auth/next-js"
 import { auth } from "@/auth/server"
 
+export const runtime = "edge"
+
 export const { GET, POST } = toNextJsHandler(auth.handler)
