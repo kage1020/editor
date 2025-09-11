4ef0ba2568b0aa85f6622033fb07733090e40025
kage1020 <4624528@ed.tus.ac.jp>
2025-09-11 14:21:07 +0900

    reset db

--- src/drizzle/0000_plain_leo.sql
+++ src/drizzle/0000_plain_leo.sql
@@ -15,6 +15,17 @@ CREATE TABLE `accounts` (
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
+CREATE TABLE `editor_contents` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text,
+	`content` text NOT NULL,
+	`json` text NOT NULL,
+	`title` text NOT NULL,
+	`created_at` integer NOT NULL,
+	`updated_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
 CREATE TABLE `passkeys` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text,
